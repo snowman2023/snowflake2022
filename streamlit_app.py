afe5c97a50436df45531a0efe51e14068da3acab
snowman2023 <121027686+snowman2023@users.noreply.github.com>
2022-12-19 12:38:28 +0100

    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title("My parents New Healty Dinner");
 streamlit.header("Breakfast menu");
-streamlit.text("Orange Julice");
+streamlit.text("Orange Juice");
 streamlit.text("Coffee");
