0ddf2bd7fb17e0ec3282abbce8d071ba6d86611c
snowman2023 <121027686+snowman2023@users.noreply.github.com>
2022-12-19 12:39:35 +0100

    Update streamlit_app.py
    Added Menu Context

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title("My parents New Healty Dinner");
-streamlit.header("Breakfast menu");
-streamlit.text("Orange Juice");
-streamlit.text("Coffee");
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
